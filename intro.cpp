29cd9941504c939198a7b850f5e3f14cc4070e0f
toby <tbcrltn@gmail.com>
2026-01-08 19:56:43 -0700

    finished first assignment

---
+++ intro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << endl;
+    cout << "The number is: " << num << endl;
+    
+    return 0;
+}
